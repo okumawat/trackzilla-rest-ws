83bb3c106f6d9fb24dce9262f29ea51a084a29fe
TestGit <omkumawat168@gmail.com>
2019-03-27 18:33:50 +0530

    added mysql jdbc details and made the util generic

--- src/main/java/com/learn/trackzilla/utils/DBUtil.java
+++ src/main/java/com/learn/trackzilla/utils/DBUtil.java
@@ -6,21 +6,25 @@ import java.util.Properties;
 
 public class DBUtil {
 
-	public static final String dbUrl = "jdbc:postgresql://localhost:5432/javaws";
-	public static final String username="postgres";
-	public static final String password="postgres";
-	
+	public static final String psqlUrl = "jdbc:postgresql://localhost:5432/javaws";
+	public static final String mysqlUrl = "jdbc:mysql://localhost:3306/javaws";
+	public static final String pusername="postgres";
+	public static final String ppassword="postgres";
+	public static final String musername="root";
+	public static final String mpassword="mysql1";
+	public static final String mysqlDriver="com.mysql.jdbc.Driver";
+	public static final String psqlDriver = "org.postgresql.Driver";
 	public static Connection getDBConnection() {
 		Connection dbConnection = null;
 		Properties props = new Properties();
-		props.put("user", username);
-		props.setProperty("password",password);
+		props.put("user", musername);
+		props.setProperty("password",mpassword);
 		//props.setProperty("ssl","true");
 		try {
-			Class.forName("org.postgresql.Driver");
-			dbConnection = DriverManager.getConnection(dbUrl, props);
+			Class.forName(mysqlDriver);
+			dbConnection = DriverManager.getConnection(mysqlUrl, props);
 			System.out.println("db connection:"+dbConnection.getSchema());
-		} catch (Exception e) {
+		} catch (Exception e) {	
 			e.printStackTrace();
 		}
 		
