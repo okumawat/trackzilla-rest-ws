2bda88aa9e09117a1bcca92441e7e5367b51f1c2
Om <omkumawat168@gmail.com>
2019-05-05 13:09:12 +0530

    heroku config

--- src/main/java/com/learn/trackzilla/utils/DBUtil.java
+++ src/main/java/com/learn/trackzilla/utils/DBUtil.java
@@ -1,5 +1,6 @@
 package com.learn.trackzilla.utils;
 
+import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,20 +13,27 @@ import javax.sql.DataSource;
 public class DBUtil {
 
 	private static DataSource ds;
-	static {
-		try {
-			Context initContext = new InitialContext();
-			Context envContext  = (Context)initContext.lookup("java:/comp/env");
-			ds = (DataSource)envContext.lookup("jdbc/javaws");
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	static {
+//		try {
+//			Context initContext = new InitialContext();
+//			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+//			ds = (DataSource)envContext.lookup("jdbc/javaws");
+//		}catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 	public static Connection getDBConnection() {
 		Connection dbConnection = null;
 		try {
-			dbConnection = ds.getConnection();
-		} catch (SQLException e) {
+			//dbConnection = ds.getConnection();
+			URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		    String username = dbUri.getUserInfo().split(":")[0];
+		    String password = dbUri.getUserInfo().split(":")[1];
+		    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+		    dbConnection =  DriverManager.getConnection(dbUrl, username, password);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("db connection successful...");
